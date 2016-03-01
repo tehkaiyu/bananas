08656077497924437622fbb88e831d3f1fd1d5b6
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-03-01 00:23:10 -0800

    Use AnimateIcon component on "join now" button and add transition to nav

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Navigation.scss';
+import AnimateIcon from '../Animate/AnimateIcon';
 import Link from '../Link';
 import Logo from '../Icon/Banana';
 
-function Navigation() {
-  return (
-    <header className="Navigation">
-      <a href="/" className="Navigation--logo">
-        <Logo />
-        Bananas
-      </a>
-      <nav className="Navigation-menu">
-        <a className="Navigation--link" href="/">About</a>
-        <a className="Navigation--link" href="/">Features</a>
-        <a className="Navigation--link" href="/">Nutrition</a>
-        <a className="Navigation--signup" href="/">Join now</a>
-      </nav>
-    </header>
-  );
+export default class extends Component {
+  render() {
+    return (
+      <header className="Navigation">
+        <a href="/" className="Navigation--logo">
+          <Logo />
+          Bananas
+        </a>
+        <nav className="Navigation-menu">
+          <a className="Navigation--link" href="/">About</a>
+          <a className="Navigation--link" href="/">Features</a>
+          <a className="Navigation--link" href="/">Nutrition</a>
+          <AnimateIcon delay={1400}>
+            <a className="Navigation--signup" href="https://itsbananas.slack.com">Join Now</a>
+          </AnimateIcon>
+        </nav>
+      </header>
+      );
+  }
 }
-
-export default Navigation;
