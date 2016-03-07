cb9b1b644bd4e9cf249029a1c49aa031ed84e0ea
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-03-06 21:17:59 -0800

    Add coming soon page

---
+++ components/ComingSoon/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ComingSoon.scss';
+
+export default class extends Component {
+  render() {
+    return (
+      <section className="Coming page-section">
+        <span className="Coming--subtitle">Events are</span>
+        <h1 className="Coming--title">Coming</h1>
+      </section>
+    );
+  }
+}
+
